5ff9a93c62bf3ad17e86ff7143a701ad70dbbd61
Santos <santicardonal777@icloud.com>
2025-12-12 09:55:43 -0500

    Inicio Repo

---
+++ src/MyAwesomeApp.tsx
@@ -0,0 +1,32 @@
+
+export const MyAwasommeApp = () => {
+
+    const name = 'Santo'
+    const lastName = 'Cardona'
+
+
+    return (
+         <>
+            <h1>{name}</h1>
+            <h3>{lastName}</h3>
+            <h2>Eres Fenomenal!!!</h2>
+        
+        </>
+
+    )
+}
+
+
+
+// export  function MyAwasommeApp () {
+
+//     return (
+//         <>
+//             <h1>Santiago</h1>
+//             <h3>Cardona</h3>
+        
+//         </>
+//     )
+
+// }
+
